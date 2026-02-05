5d81ca7fe63bd7bfd70ef5f7578f3f4be6d0c0e6
Ahmad Abdul Rahman <akramahmad060@gmail.com>
2026-02-05 16:51:52 +0200

    Fix import path for User model in route.js

--- src/app/api/users/[id]/route.js
+++ src/app/api/users/[id]/route.js
@@ -1,5 +1,5 @@
 import dbConnect from "../../../lib/mongodb";
-import User from "@/models/User";
+import User from "../../../models/User";
 
 export async function DELETE(req, { params }) {
     const { id } = params;
